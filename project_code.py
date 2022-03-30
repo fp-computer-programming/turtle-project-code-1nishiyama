29295d0a254cb454fa609c27fbafabaa98c6f06b
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-03-30 10:11:56 -0400

    revise project_code.py

--- project_code.py
+++ project_code.py
@@ -3,36 +3,59 @@
 import turtle
 
 window = turtle.Screen()
-
+turtle.screensize(500, 500)
 t = turtle.Turtle()
 t.hideturtle()
+t.pensize(3)
+
+length = window.numinput("Length", "Please choose the length for the base to be (px): ")
 
 def draw_base():
-    t.pensize(window.numinput("Width", "Please choose how wide for the base to be (px): "))
-    t.fillcolor(window.textinput("Color", "Please choose a base color: "))
     t.begin_fill()
     for x in range(4):
         t.right(90)
-        t.forward(100)
+        t.forward(length)
+    t.fillcolor(window.textinput("Color", "Please choose a base color: "))
     t.end_fill()
     t.penup()
 
 
-# first roof type
 def type_a():
-    t.pensize(window.numinput("Width", "Please choose how wide for the roof to be (px): "))
-    t.fillcolor(window.textinput("Color", "Please choose a roof color: "))
     t.begin_fill()
-    t.goto(-100, 0)
+    t.goto(-(length), 0)
+    a1 = window.numinput("Side 1", "Please choose the angle for the roof: ")
     t.pendown()
-    t.left(45)
-    t.forward(71)
-    t.right(90)
-    t.forward(71)
+    t.left(a1)
+    t.forward(a1)
+    t.goto(0, 0)
+    t.fillcolor(window.textinput("Color", "Please choose a roof color: "))
     t.end_fill()
 
 
+def type_b():
+    t.begin_fill()
+    t.goto(-(length), 0)
+    a1 = window.numinput("Side 1", "Please choose the angle for left side of the roof (0-90): ")
+    t.pendown()
+    t.left(a1)
+    t.forward(length)
+    a2 = window.numinput("Side 2", "Please choose the angle for right side of the roof (0-90): ")
+    t.left(a2)
+    t.goto(0, 0)
+    t.fillcolor(window.textinput("Color", "Please choose a roof color: "))
+    t.end_fill()
+    t.penup()
+
+def door():
+    t.goto(-(length / 2), 0)
+    t.pencolor("brown")
+    for x in range(2):
+        t.left(90)
+        t.forward(length / 8)
+    
+
 draw_base()
-type_a()
+type_b()
+door()
 
 window.mainloop()
