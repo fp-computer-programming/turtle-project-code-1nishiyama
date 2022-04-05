91156d16cba3bd0ff3286b22ec0e29a87023f747
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-04-05 10:35:33 -0400

    add doorknob revise type_2 draw_driveway

--- counter.py
+++ counter.py
@@ -1,41 +1,26 @@
-# Author: ATN 3/28/22
-
 import turtle
-
-# setting up the screen size for the player
 window = turtle.Screen()
 window.setup(500, 500)
-
-# defining all three turtles for their creations. each turtle will be hidden with the same pen size for consistency
 a = turtle.Turtle()
 a.hideturtle()
 a.pensize(3)
-
 b = turtle.Turtle()
 b.hideturtle()
 b.pensize(3)
-
 c = turtle.Turtle()
 c.hideturtle()
 c.pensize(3)
-
 d = turtle.Turtle()
 d.hideturtle()
 d.pensize(3)
-
-# light blue bg for an accurate "sky"
 window.bgcolor('light blue')
-# 100-150px will be the best size relative to the door
 length = window.numinput("Length", "Please choose the length for the base to be (100-150px): ")
-
-# function for the door
 def draw_door():
     a.penup()
     a.goto(-12.5, 0)
     a.pendown()
     a.begin_fill()
     a.fillcolor('#654321')
-    # constructing the shape in the most efficient way that i saw fit
     for x in range(2):
         a.right(90)
         a.forward(35)
@@ -50,8 +35,6 @@ def draw_door():
     d.circle(3)
     d.end_fill()
     d.penup()
-
-# for the second option of a door, i found that it wouldn't function properly without writing it out as such
 def draw_double_door():
     a.penup()
     a.goto(-25, 0)
@@ -90,8 +73,6 @@ def draw_double_door():
     d.circle(3)
     d.end_fill()
     d.penup()
-
-# constructing the basic shape of the house 
 def draw_base():
     a.speed(3)
     a.begin_fill()
@@ -108,8 +89,6 @@ def draw_base():
     a.fillcolor(window.textinput("Color", "Please choose a base color: "))
     a.end_fill()
     draw_bg()
-
-# i drew a nice basic background using the fastest speed offered in order make it less boring for the user
 def draw_bg():
     a.speed(0)
     a.goto(-500, -500)
@@ -118,14 +97,10 @@ def draw_bg():
     a.goto(-500, 0)
     a.goto(500, 0)
     a.goto(500, -500)
-    # light green color for the "grass"
     a.fillcolor('light green')
     a.end_fill()
     a.penup()
-    # the speed changes back to it's default
     a.speed(3)
-
-# defines the first type of roof 
 def type_1():
     a.penup()
     a.goto(-(length / 2), length / 1.5)
@@ -136,8 +111,6 @@ def type_1():
     a.goto(length / 2, length / 1.5)
     a.fillcolor(window.textinput("Color", "Please choose a roof color: "))
     a.end_fill()
-
-# defines the second type of roof
 def type_2():
     a.penup()
     a.goto(-(length / 2), length / 1.5)
@@ -148,9 +121,6 @@ def type_2():
     a.goto(length / 2, length / 1.5)
     a.fillcolor(window.textinput("Color", "Please choose a roof color: "))
     a.end_fill()
-
-# i tried to do some perspective work in creating these trees because it is extremely difficult and time-consuming
-# to make detailed art
 def draw_trees():
     b.penup()
     b.goto(-100, 0)
@@ -186,8 +156,6 @@ def draw_trees():
     b.fillcolor('green')
     b.circle(25)
     b.end_fill()
-
-
 def draw_driveway():
     c.penup()
     c.goto(175, 0)
@@ -204,9 +172,6 @@ def draw_driveway():
     c.left(105)
     c.forward(50)
     c.end_fill()
-    
-
-# i called the draw_base function to begin the program, then asked the user the following conditionals
 draw_base()
 num_doors = window.numinput("Doors", "Please choose '1' or '2' doors: ")
 if num_doors == 1:
@@ -224,5 +189,4 @@ if trees.lower() == 'y':
 driveway = window.textinput("Driveway", "Would you like a driveway? (Y/N) ")
 if driveway.lower() == 'y':
     draw_driveway()
-    
 window.mainloop()
